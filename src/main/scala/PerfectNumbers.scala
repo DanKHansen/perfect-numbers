7e3937bfe7b455edc9af5f3dea83d56cbadb0d48
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-15 14:19:39 +0200

    refactor

--- src/main/scala/PerfectNumbers.scala
+++ src/main/scala/PerfectNumbers.scala
@@ -2,12 +2,15 @@ import NumberType.{Abundant, Deficient, Perfect}
 
 enum NumberType:
    case Perfect, Abundant, Deficient
+
 object PerfectNumbers:
    def classify(n: Int): Either[String, NumberType] =
       if n <= 0 then Left("Classification is only possible for natural numbers.")
+      else if n == 1 then Right(Deficient)
       else
-         lazy val aliqout = (1 until n).filter(i => n % i == 0).sum
-         aliqout match
-            case p if p == n => Right(Perfect)
-            case a if a > n  => Right(Abundant)
-            case d if d < n  => Right(Deficient)
+         val sum = (1 to math.sqrt(n).toInt).foldLeft(0) { (s, i) =>
+            s + (if n % i == 0 then i + (if i != 1 && i != n / i then n / i else 0) else 0)
+         }
+         if sum == n then Right(Perfect)
+         else if sum > n then Right(Abundant)
+         else Right(Deficient)
