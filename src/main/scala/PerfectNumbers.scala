88f3aeb87ece70638ad218422e9ff3b7e5c4241a
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-15 14:34:51 +0200

    fewest lines

--- src/main/scala/PerfectNumbers.scala
+++ src/main/scala/PerfectNumbers.scala
@@ -1,16 +1,10 @@
-import NumberType.{Abundant, Deficient, Perfect}
-
+object PerfectNumbers:
+   def classify(n: Int): Either[String, NumberType] = if n <= 0 then
+      Left("Classification is only possible for natural numbers.")
+   else
+      (1 until n).filter(i => n % i == 0).sum match
+         case p if p == n => Right(NumberType.Perfect)
+         case a if a > n  => Right(NumberType.Abundant)
+         case _           => Right(NumberType.Deficient)
 enum NumberType:
    case Perfect, Abundant, Deficient
-
-object PerfectNumbers:
-   def classify(n: Int): Either[String, NumberType] =
-      if n <= 0 then Left("Classification is only possible for natural numbers.")
-      else if n == 1 then Right(Deficient)
-      else
-         val sum = (1 to math.sqrt(n).toInt).foldLeft(0) { (s, i) =>
-            s + (if n % i == 0 then i + (if i != 1 && i != n / i then n / i else 0) else 0)
-         }
-         if sum == n then Right(Perfect)
-         else if sum > n then Right(Abundant)
-         else Right(Deficient)
